e7e46d3511e77910dd86703ae622df4b9a6d5419
HuangWen8078 <billyHuang8078@gmail.com>
2021-10-20 00:16:11 +0800

    #refs_test commit

--- Learn-Spring/src/main/java/Learn/LearnSpringApplication.java
+++ Learn-Spring/src/main/java/Learn/LearnSpringApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LearnSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearnSpringApplication.class, args);
+		SpringApplication.run(LearnSpringApplication.class, args);   
 	}
 
 }
