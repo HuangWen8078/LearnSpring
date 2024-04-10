c28f882615abad605e9b9881ecf141b2c5074dee
billy <billy.huang@riversoft.com.tw>
2024-04-10 12:03:50 +0800

    :rocket: 加上Spring security

---
+++ Learn-Spring/src/main/java/Learn/HelloWorld.java
@@ -0,0 +1,13 @@
+package Learn;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+	@GetMapping("/")
+	public String HelloWorld(){
+		return "Hey , 恭喜啟動成功";
+	}
+}
